c586693e4cd201dc8cf9d470464c4c46e50081db
Kai Sun <sunkaicn@gmail.com>
2015-11-01 22:20:11 -0500

    100 label

--- Reddit/label.py
+++ Reddit/label.py
@@ -18,19 +18,38 @@ class ConcatJSONDecoder(json.JSONDecoder):
             yield obj
 
 def analyse():
-    f = open("label_a.json", "r")
+    f = open("label.json", "r")
     data = json.load(f)
     f.close()
-    cnt = {'a':0, 'b':0, 'c':0}
+    print len(data)
+    cnt1 = {'a':0, 'b':0, 'c':0}
+    cnt2 = {'-':0, '0':0, '+':0}
     cor = {'a':0, 'b':0, 'c':0}
+    avgl = 0
     for i in xrange(len(data)):
-        if data[i][2]["neg"] == data[i][2]["pos"]: continue
-        cnt[data[i][0]] += 1
+        #if data[i][2]["neg"] == data[i][2]["pos"]: continue
+        avgl += len(data[i][1].split('.'))
+        cnt1[data[i][0]] += 1
         if data[i][2]["compound"] > 0 and data[i][0] == 'c':
             cor['c'] += 1
         elif data[i][2]["compound"] < 0 and data[i][0] == 'a':
             cor['a'] += 1
-    print cnt
+        elif data[i][2]["compound"] == 0 and data[i][0] == 'b':
+            cor['b'] += 1
+            
+        if data[i][2]["compound"] < 0:
+            cnt2['-'] += 1
+        elif data[i][2]["compound"] == 0:
+            cnt2['0'] += 1
+        else:
+            cnt2['+'] += 1
+
+        s = {'a':-1, 'b':0, 'c':1}
+        #print str(s[data[i][0]]), str(data[i][2]["compound"])
+
+    print float(avgl) / len(data)
+    print cnt1
+    #print cnt2
     print cor
 
 def main():
@@ -47,12 +66,22 @@ def main():
     random.shuffle(text)
     ld = []
     p = 0
+    vis = {}
+    try:
+        f = open("label.json", "r")
+        ld = json.load(f)
+        f.close()
+        for x in ld:
+            vis[x[1]] = True
+    except:
+        pass
     while True:
         if p >= len(text): break
-        if text[p] is not None:
+        if text[p] is not None and text[p] not in vis:
             print text[p]
             r = raw_input('a. -1  b. 0  c. 1')
             ld += [(r, text[p], vaderSentiment(text[p].encode('utf-8')))]
+            vis[text[p]] = True
             print ld[-1][0], ld[-1][2]
             print '\n\n\n====================\n'
         f = open("label.json", "w")
@@ -62,5 +91,5 @@ def main():
     
 
 if __name__ == '__main__':
-    #main()
     analyse()
+    main()
