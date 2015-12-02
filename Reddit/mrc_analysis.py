065d0cd780f3d3bdb273e863c05114510f9de593
Kai Sun <sunkaicn@gmail.com>
2015-12-02 15:25:34 -0500

    update mrc_stat

--- Reddit/mrc_analysis.py
+++ Reddit/mrc_analysis.py
@@ -1,5 +1,5 @@
 def main():
-    f = open("stats.txt", "r")
+    f = open("stats_mrc.txt", "r")
     h = f.readline()
     h = h.split()[1:]
     r = {}
@@ -12,16 +12,21 @@ def main():
         for i in xrange(len(h)):
             r[t][h[i]] = float(x[i])
         x = f.readline()
+    f.close()
+    
     stat = {}
     statavg = {}
     statvar = {}
+    delta = {}
     for i in xrange(14):
         stat[i] = [0, 0]
         statavg[i] = [0, 0]
         statvar[i] = [0, 0]
+        delta[i] = []
         for x in r:
             a = 'MRC_A_'+str(i)
             q = 'MRC_Q_'+str(i)
+            if r[x][a] == 0 or r[x][q] == 0: continue
             if r[x][a] > r[x][q]:
                 stat[i][0] += 1
             elif r[x][a] < r[x][q]:
@@ -30,6 +35,8 @@ def main():
             statavg[i][1] += r[x][q]
             statvar[i][0] += r[x][a] ** 2
             statvar[i][1] += r[x][q] ** 2
+            delta[i] += [(r[x][a] - r[x][q])] #[(r[x][a] - r[x][q]) / (r[x][a] + r[x][q])]
+            
         statavg[i][0] /= float(len(r))
         statavg[i][1] /= float(len(r))
         statvar[i][0] = (statvar[i][0] / float(len(r)) - statavg[i][0] ** 2) ** 0.5
@@ -41,6 +48,15 @@ def main():
     print statvar
     print
     print len(r)
+
+    f = open("mrc.csv", "w")
+    for i in xrange(14):
+        delta[i].sort()
+        for j in xrange(len(delta[i])):
+            f.write(str(delta[i][j]) + ', ')
+        f.write("\n")
+        
+    f.close()
 
 if __name__ == '__main__':
     main()
